a23e05488bf5e3f7f77b3c16d4509731e86c4a60
alfie <alfiepogado@gmail.com>
2026-04-04 22:13:16 +0800

    fix topup

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -7,21 +7,40 @@ use App\Services\Contracts\ProfileServiceInterface;
 
 class ProfileService implements ProfileServiceInterface
 {
+    /**
+     * Get Active Profile id
+     *
+     * @param User $user
+     * @return integer|null
+     */
     public function getActiveProfileId(User $user): ?int
     {
         $token = $user->currentAccessToken();
 
         if (!$token) return null;
 
+        // Return token-based active profile
         foreach ($token->abilities as $ability) {
             if (str_starts_with($ability, 'profile:')) {
                 return (int) str_replace('profile:', '', $ability);
             }
         }
 
-        return null;
+        // Return fallback user’s selected/default profile
+        if ($user->active_profile_id) {
+            return $user->active_profile_id;
+        }
+
+        // Otherwise return the latest profile
+        return $user->profiles()->latest()->value('id');
     }
 
+    /**
+     * Check if it's using profile
+     *
+     * @param User $user
+     * @return boolean
+     */
     public function isUsingProfile(User $user): bool
     {
         return $this->getActiveProfileId($user) !== null;
